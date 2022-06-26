0a74bb98c786e8f5319d186cb6802fa0f0fcc971
light0419 <763198261@qq.com>
2022-06-27 03:03:21 +0800

    除了关于我们其他的完成了

--- js/js.js
+++ js/js.js
@@ -1,7 +1,21 @@
 // JavaScript Document
 $(function(){
+    $('.index_nav .chacha').click(function(){
+        $('.index_nav').fadeOut();
+    })
+    $('.header .menu1').click(function(){
+        $('.index_nav').fadeIn();
+    })
 
-
-
-   
+   $(window).scroll(function(){
+    if($(window).scrollTop()>10){
+        $('.header').addClass('on');
+    }
+    else{
+        $('.header').removeClass('on');
+    }
+   })
+   if($(window).width()>768){
+    $('.ejbanner').height($(window).height());
+   }
 }) 
